29c3dd9a5beed3ce9291d30759e2571de3ef01ba
josedavila7452 <67973259+josedavila7452@users.noreply.github.com>
2023-03-04 18:03:21 -0500

    1 file modified, 3 added

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.component.html',
+  styles: ['./app.component.css; }']
+})
+export class App {
+  name = 'Angddddddular';
+}
